44cb84ef34723fa8f5d6b60debb73c61aa358d6d
Scott Sargent <ssa@dynamicwebusa.com>
2017-08-26 12:05:40 -0400

    Better repositories

--- src/BistroFiftyTwo.Server/Repositories/StepRepository.cs
+++ src/BistroFiftyTwo.Server/Repositories/StepRepository.cs
@@ -9,15 +9,63 @@ using BistroFiftyTwo.Server.Services;
 
 namespace BistroFiftyTwo.Server.Repositories
 {
-    public class StepRepository : AutomaticDataRepository<Step>, IStepRepository
+    public class StepRepository : IStepRepository
     {
-        public StepRepository(IConfigurationService configurationService) : base(configurationService)
+        public StepRepository(IConfigurationService configurationService) 
         {
+            Connection = new NpgsqlConnection(configurationService.Get("Data:Recipe:ConnectionString"));
+            Connection.Open();
+        }
+
+        protected NpgsqlConnection Connection { get; set; }
+        public async Task<Step> CreateAsync(Step item)
+        {
+            var query =
+                "insert into recipe_steps (ordinal, recipeid, instructions, createdby, modifiedby) values (@ordinal, @recipeid, @instructions, @createdbby, @modifiedby) returning *";
+
+            var record = new
+            {
+                ordinal = item.Ordinal,
+                recipeid = item.RecipeId,
+                instructions = item.Instructions,
+                createdby = item.CreatedBy,
+                modifiedby = item.ModifiedBy
+            };
+
+            return await Connection.QuerySingleAsync<Step>(query, record);
+        }
+
+        public async Task DeleteAsync(Step item)
+        {
+            var query = "delete from recipe_steps where id = @id";
+
+            await Connection.ExecuteAsync(query, new { id = item.ID });
+        }
+
+        public async Task<IEnumerable<Step>> GetAllAsync()
+        {
+            var query =
+                "select id, ordinal, recipeid, instructions, createddate, createdby, modifieddate, modifiedby from recipe_steps";
+
+            return await Connection.QueryAsync<Step>(query);
+        }
+
+        public async Task<Step> GetAsync(Guid id)
+        {
+            var query =
+                "select id, ordinal, recipeid, instructions, createddate, createdby, modifieddate, modifiedby from recipe_steps id = @id";
+            
+            return await Connection.QuerySingleAsync<Step>(query, new {id});
         }
 
         public async Task<IEnumerable<Step>> GetByRecipeIdAsync(Guid recipeId)
         {
             return await Connection.QueryAsync<Step>("select * from recipe_steps where recipeid = @recipeId", new { recipeId });
+        }
+
+        public Task<Step> UpdateAsync(Step item)
+        {
+            throw new NotImplementedException();
         }
     }
 }
