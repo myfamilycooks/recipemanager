6f0ee01308bcb56631dc35f8325106528fe461b7
Scott Sargent <scott.sargent@gmail.com>
2018-01-28 17:41:42 -0500

    Upgrading jquery to prevent security issue

---
+++ src/BistroFiftyTwo.Api/Controllers/TokenController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BistroFiftyTwo.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Token")]
+    public class TokenController : Controller
+    {
+    }
+}
