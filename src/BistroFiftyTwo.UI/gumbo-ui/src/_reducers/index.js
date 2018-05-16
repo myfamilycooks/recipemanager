c851db3ebeefc475557485942c721a485a16bd30
Scott Sargent <scott.sargent@gmail.com>
2018-05-16 08:16:22 -0400

    adding structure for gumbo-ui

---
+++ src/BistroFiftyTwo.UI/gumbo-ui/src/_reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+/*
+import { authentication } from './authentication.reducer';
+import { registration } from './registration.reducer';
+import { users } from './users.reducer';
+import { alert } from './alert.reducer';
+*/
+const rootReducer = combineReducers({
+});
+
+export default rootReducer;
--- src/BistroFiftyTwo.UI/gumbo-ui/src/index.js
+++ src/BistroFiftyTwo.UI/gumbo-ui/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { App } from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+// based on http://jasonwatmore.com/post/2017/09/16/react-redux-user-registration-and-login-tutorial-example
