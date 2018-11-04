822a8d213ea6037cae47801214be6217a6fea7a0
Scott Sargent <ssa@dynamicwebusa.com>
2018-11-04 16:26:17 -0500

    Upgrading packages due to github reccomendations

--- src/BistroFiftyTwo.WebApp/app/actions/types.js
+++
@@ -1,7 +0,0 @@
-export const AUTH_USER = 'auth_user';
-export const DEAUTH_USER = 'unauth_user';
-export const LOGIN_ERROR = 'login_error';
-export const LOAD_PROFILE = 'load_profile';
-export const PROFILE_IS_LOADING = 'profile_is_loading';
-export const PROFILE_HAS_LOADED = 'profile_has_loaded';
-export const PROFILE_HAS_ERRORED = 'profile_has_errored';
